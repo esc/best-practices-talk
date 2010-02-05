175b361c599abc131f8f4ce60d627b2e874319e3
Valentin Haenel <valentin.haenel@gmx.de>
2010-02-05 17:14:42 +0100

    add missing images and example code

---
+++ example-test2.py
@@ -0,0 +1,11 @@
+import unittest
+
+def my_sum(my_list):
+    return sum(my_list)
+
+class Test(unittest.TestCase):
+    def test_my_sum(self):
+        self.assertEqual(my_sum([1,2,3]),6)
+
+if __name__ == "__main__":
+    unittest.main()
