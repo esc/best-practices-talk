5e1d837a3ddc71665e0704efdbb7a4a8ad3e3bdf
Valentin Haenel <valentin.haenel@gmx.de>
2010-09-23 14:27:10 +0200

    refactor slide on refactoring

---
+++ my_product_refactor.py
@@ -0,0 +1,13 @@
+import numpy
+
+def my_product(numbers):
+    """ Compute the product of a list of numbers. """
+    total = 1
+    for i in li:
+        total *= i
+    return total
+
+
+def my_product_refactor(numbers):
+    return numpy.prod(numbers)
+
