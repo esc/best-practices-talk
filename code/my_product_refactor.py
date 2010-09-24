caf8b1887a46e67b52a4e35119e326c8c1331ca8
Valentin Haenel <valentin.haenel@gmx.de>
2010-09-24 14:02:46 +0200

    fix example code

--- code/my_product_refactor.py
+++ code/my_product_refactor.py
@@ -1,14 +1,14 @@
 import numpy
 
 def my_product(numbers):
-    """ Compute the product of a list of numbers. """
+    """ Compute the product of a sequence of numbers. """
     total = 1
-    for i in li:
-        total *= i
+    for item in numbers:
+        total *= item
     return total
 
 
 def my_product_refactor(numbers):
-    """ Compute the product of a list of numbers. """
+    """ Compute the product of a sequence of numbers. """
     return numpy.prod(numbers)
 
--- code/my_product_test.py
+++ code/my_product_test.py
@@ -1,12 +1,13 @@
 import nose.tools as nt
 
-def my_product(li):
-    """ Compute the product of a list of numbers. """
+def my_product(numbers):
+    """ Compute the product of a sequence of numbers. """
     total = 1
-    for i in li:
-        total *= i
+    for item in numbers:
+        total *= item
     return total
+
 
 def test_my_product():
     """ Test my_product() on simple case. """
-    nt.assert_equal(my_product([1,2,3]),6)
+    nt.assert_equal(my_product([1, 2, 3]), 6)
