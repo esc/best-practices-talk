6bd474654cb5764170e53710b79e9793d3f0c3f4
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-01 17:04:24 +0200

    refactor test section

--- code/my_product.py
+++ code/my_product.py
@@ -1,6 +1,10 @@
+from numbers import Number
+
 def my_product(numbers):
     """ Compute the product of a sequence of numbers. """
     total = 1
     for item in numbers:
+        if not isinstance(item, Number):
+            raise TypeError("%r unsupported by 'my_product'" %type(item))
         total *= item
     return total
--- code/my_product_test.py
+++ code/my_product_test.py
@@ -1,13 +1,11 @@
 import nose.tools as nt
-
-def my_product(numbers):
-    """ Compute the product of a sequence of numbers. """
-    total = 1
-    for item in numbers:
-        total *= item
-    return total
-
+from my_product import my_product
 
 def test_my_product():
     """ Test my_product() on simple case. """
     nt.assert_equal(my_product([1, 2, 3]), 6)
+
+def test_raise_type_error():
+    """ Test that my_product raises a type error. """
+    nt.assert_raises(TypeError, my_product, ['abc', 1, 2, 3])
+
