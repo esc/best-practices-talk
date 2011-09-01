6c71802b5ed07906ec037610c70f5acaef37b672
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-01 18:09:46 +0200

    clean refactoring examples

--- code/my_product_duplication.py
+++ code/my_product_duplication.py
@@ -1,13 +1,5 @@
 import numpy
 
-def my_product(numbers):
-    """ Compute the product of a sequence of numbers. """
-    total = 1
-    for item in numbers:
-        total *= item
-    return total
-
-
 def my_product_refactor(numbers):
     """ Compute the product of a sequence of numbers. """
     return numpy.prod(numbers)
