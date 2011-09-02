c1d58fbd355f169dfb7c97baff0a7a899bf7a333
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-02 12:24:53 +0200

    overhaul testing section

---
+++ code/my_product_fixed.py
@@ -0,0 +1,11 @@
+from numbers import Number
+
+def my_product(numbers):
+    """ Compute the product of a sequence of numbers. """
+    total = 1
+    for item in numbers:
+        if not isinstance(item, Number):
+            raise TypeError("%r unsupported by 'my_product'"
+                    %type(item))
+        total *= item
+    return total
