890f9bb05c54e81d583c0c3fb968a273c4ae8ef7
Valentin Haenel <valentin.haenel@gmx.de>
2011-09-02 11:36:41 +0200

    rename my_product_naive.py

--- code/my_product.py
+++ code/my_product.py
@@ -1,10 +1,6 @@
-from numbers import Number
-
 def my_product(numbers):
     """ Compute the product of a sequence of numbers. """
     total = 1
     for item in numbers:
-        if not isinstance(item, Number):
-            raise TypeError("%r unsupported by 'my_product'" %type(item))
         total *= item
     return total
--- code/my_product_naive.py
+++
@@ -1,6 +0,0 @@
-def my_product(numbers):
-    """ Compute the product of a sequence of numbers. """
-    total = 1
-    for item in numbers:
-        total *= item
-    return total
